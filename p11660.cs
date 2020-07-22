3e9cd5e07edd3a32981e9a683d35844f3a536ca2
jihoon-jang <55784520+jihoon-jang@users.noreply.github.com>
2020-07-22 18:45:32 +0900

    Add files via upload

---
+++ p11660.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0722
+{
+    class p11660
+    {
+        static int n, m;
+        static int[,] map;
+        static int[,] dp;
+        static StringBuilder sb = new StringBuilder();
+        static void Main()
+        {
+            String[] s = Console.ReadLine().Split(' ');
+            n = int.Parse(s[0]);
+            m = int.Parse(s[1]);
+            map = new int[n + 1, n + 1];
+            dp = new int[n + 1, n + 1];
+
+            for (int i = 1; i <= n; i++)
+            {
+                s = Console.ReadLine().Split(' ');
+                for (int j = 1; j <= n; j++) { 
+                    map[i, j] = int.Parse(s[j - 1]);
+                    dp[i, j] = dp[i - 1, j] + dp[i, j - 1] + map[i, j] - dp[i - 1, j - 1];
+                }
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                sum();
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+
+        static void sum()
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            int x1 = int.Parse(s[0]);
+            int y1 = int.Parse(s[1]);
+            int x2 = int.Parse(s[2]);
+            int y2 = int.Parse(s[3]);
+
+            sb.AppendLine((dp[x2, y2] - dp[x1 - 1, y2] - dp[x2, y1 - 1] + dp[x1 - 1, y1 - 1]).ToString());
+        }
+    }
+}
--- p2206.cs
+++ p2206.cs
@@ -10,6 +10,7 @@ namespace _0721
         static short[] dx = { -1, 1, 0, 0 };
         static short[] dy = { 0, 0, -1, 1 };
         static int[,] map;
+        static bool[,,] visit;
         static Queue<Point> q = new Queue<Point>();
         static void Main()
         {
@@ -17,6 +18,7 @@ namespace _0721
             n = short.Parse(s[0]);
             m = short.Parse(s[1]);
             map = new int[n + 1, m + 1];
+            visit = new bool[n + 1, m + 1, 2];
 
             for (int i = 1; i <= n; i++)
             {
@@ -26,14 +28,25 @@ namespace _0721
                     map[i, j] = str[j - 1] - '0';
                 }
             }
-            q.Enqueue(new Point(1, 1, true, 1));
+
+            bfs();
+        }
+
+        static void bfs()
+        {
+            int answer = 0;
+            q.Enqueue(new Point(1, 1, 1, 1));
 
             while (q.Count != 0)
             {
                 Point p = q.Dequeue();
-                map[p.x, p.y] = p.distance;
+
                 if (p.x == n && p.y == m)
+                {
+                    answer = p.distance;
                     break;
+                }
+
                 for (short i = 0; i < 4; i++)
                 {
                     short nx = (short)(p.x + dx[i]);
@@ -41,19 +54,22 @@ namespace _0721
 
                     if (nx >= 1 && ny >= 1 && nx <= n && ny <= m)
                     {
-                        if (map[nx, ny] == 0)
+                        if (map[nx, ny] == 0 && !visit[nx, ny, p.chance])
                         {
-                            q.Enqueue(new Point(nx, ny, p.chance, map[p.x, p.y] + 1));
+                            visit[nx, ny, p.chance] = true;
+                            q.Enqueue(new Point(nx, ny, p.chance, p.distance + 1));
                         }
-                        else if (map[nx, ny] == 1 && p.chance)
+                        else if (map[nx, ny] == 1 && p.chance == 1 && !visit[nx, ny, 0])
                         {
-                            q.Enqueue(new Point(nx, ny, false, map[p.x, p.y] + 1));
+                            visit[nx, ny, 0] = true;
+                            q.Enqueue(new Point(nx, ny, 0, p.distance + 1));
                         }
                     }
                 }
                 p = null;
             }
-            Console.WriteLine(map[n, m] == 0 ? -1 : map[n, m]);
+
+            Console.WriteLine(answer == 0 ? -1 : answer);
         }
 
     }
@@ -61,9 +77,9 @@ namespace _0721
     class Point
     {
         public short x, y;
-        public bool chance;
+        public int chance;
         public int distance;
-        public Point(short x, short y, bool c, int d)
+        public Point(short x, short y, int c, int d)
         {
             this.x = x;
             this.y = y;
