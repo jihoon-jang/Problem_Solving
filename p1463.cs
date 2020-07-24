a910829b1eca666126482539a76ec26da796c14e
jihoon-jang <55784520+jihoon-jang@users.noreply.github.com>
2020-07-24 18:45:02 +0900

    Add files via upload

---
+++ p1463.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0724
+{
+    class p1463
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] dp = new int[1000001];
+
+            if (n >= 2)
+                dp[2] = 1;
+            if (n >= 3)
+                dp[3] = 1;
+
+            if (n >= 4)
+                for (int i = 4; i <= n; i++)
+                {
+                    dp[i] = dp[i - 1] + 1;
+                    if (dp[i] % 3 == 0)
+                        dp[i] = dp[i / 3] + 1;
+                    Console.WriteLine(dp[5]);
+                    if (dp[i] % 2 == 0)
+                        dp[i] = dp[i / 2] + 1;
+                }
+
+            Console.WriteLine(dp[n]);
+        }
+    }
+}
--- p1904.cs
+++ p1904.cs
@@ -1,21 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace ConsoleApp1
+namespace _0724
 {
     class p1904
     {
-		static void Main(String[] args)
-		{
-			int n = int.Parse(Console.ReadLine());
-			int[] dp = new int[1000001];
-			dp[1] = 1;
-			dp[2] = 2;
-
-			for (int i = 3; i <= n; i++)
-				dp[i] = (dp[i - 1] + dp[i - 2]) %15746;
-
-			Console.WriteLine(dp[n]);
-	            
-		}
-	}
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] dp = new int[n + 1];
+            dp[1] = 1;
+            if (n >= 2)
+                dp[2] = 2;
+            if (n >= 3)
+            {
+                for(int i = 3; i <= n; i++)
+                {
+                    dp[i] = (dp[i - 1] + dp[i - 2])%15746;
+                }
+            }
+            Console.WriteLine(dp[n]);
+        }
+    }
 }
