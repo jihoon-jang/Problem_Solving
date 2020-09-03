17a53d4eb5a20038a7b56c0d54c057ec51703661
jihoon-jang <55784520+jihoon-jang@users.noreply.github.com>
2020-09-03 23:24:56 +0900

    200903

--- pm동굴탐험.java
+++ pm동굴탐험.java
@@ -2,58 +2,61 @@ import java.util.*;
 
 class Solution {
 	
-	int[] prior, ret;
-	boolean[] visit;
-	ArrayList<Integer>[] list;
-
-	public boolean solution(int n, int[][] path, int[][] order) {
-		prior = new int[n + 1];
-		ret = new int[n + 1];
-		list = new ArrayList[n + 1];
-		visit = new boolean[n + 1];
-		
-		for (int i = 0; i < n + 1; ++i)
-			list[i] = new ArrayList<>();
-		
-		for (int i = 0; i < path.length; ++i) {
-			list[path[i][0]].add(path[i][1]);
-			list[path[i][1]].add(path[i][0]);
-		}
-		// prior[1] = 4, prior[7] = 8
-		for (int i = 0; i < order.length; ++i) {
-			prior[order[i][1]] = order[i][0];
-		}
-		
-		if (prior[0] != 0)
-			return false;
-		
-		visit[0] = true;
-		Stack<Integer> st = new Stack<>();
-		
-		for (int i = 0; i < list[0].size(); ++i) {
-			st.push(list[0].get(i));
-		}
-		
-		while (!st.isEmpty()) {
-			int v = st.pop();
-			if (visit[v])
-				continue;
-			if (!visit[prior[v]]) {
-				ret[prior[v]] = v;
-				continue;
-			}
-			visit[v] = true;
-			st.push(ret[v]);
-			for (int i = 0; i < list[v].size(); ++i) {
-				st.push(list[v].get(i));
-			}
-		}
-
-		for (int i = 0; i < n; ++i) {
-			if (!visit[i]) {
-				return false;
-			}
-		}
-		return true;
-	}
+	public int n;
+	public int before[], after[];
+	public ArrayList<Integer> al[];
+	public boolean visit[];
+	
+    public boolean solution(int n, int[][] path, int[][] order) {
+    	this.n = n;
+    	before = new int[n];
+    	after = new int[n];
+    	visit = new boolean[n];
+    	al = new ArrayList[n];
+    	
+    	for(int i = 0; i < n; i++)
+    		al[i] = new ArrayList<Integer>();
+    	
+    	for(int i = 0; i < path.length; i++) {
+    		int a = path[i][0];
+    		int b = path[i][1];
+    		al[a].add(b);
+    		al[b].add(a);
+    	}
+    	
+    	for(int i = 0; i < order.length; i++)
+    		before[order[i][1]] = order[i][0];
+    	
+    	if(before[0] != 0)
+    		return false;
+    	
+    	Stack<Integer> st = new Stack<Integer>();
+    	visit[0] = true;
+    	
+    	for(int i = 0; i < al[0].size(); i++)
+    		st.add(al[0].get(i));
+    	
+    	while(!st.isEmpty()) {
+    		int now = st.pop();
+    		
+    		if(visit[now])
+    			continue;
+    		if(!visit[before[now]]) {
+    			after[before[now]] = now;
+    			continue;
+    		}
+    		
+    		visit[now] = true;
+    		for(int i = 0; i < al[now].size(); i++)
+    			st.add(al[now].get(i));
+    		st.add(after[now]);
+    	}
+    	
+        for(int i = 0; i < n; i++)
+        	if(!visit[i])
+        		return false;
+        
+        return true;
+    }
+    
 }
