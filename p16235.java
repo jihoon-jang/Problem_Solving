94f47889d3ab5d1f415e17d021eb4c111a3dec0d
jihoon-jang <55784520+jihoon-jang@users.noreply.github.com>
2020-09-09 13:29:05 +0900

    200909

--- p16235.java
+++ p16235.java
@@ -3,126 +3,88 @@ import java.io.*;
 
 public class p16235 {
 
-	public static int n, k;
-	public static Tree map[][];
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer stz = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(stz.nextToken());
+		int n = Integer.parseInt(stz.nextToken());
 		int m = Integer.parseInt(stz.nextToken());
-		k = Integer.parseInt(stz.nextToken());
-		map = new Tree[n][n];
+		int k = Integer.parseInt(stz.nextToken());
+		int map[][] = new int[n][n];
 		int a[][] = new int[n][n]; 
-		int answer = 0;
 
 		for(int i = 0; i < n; i++) {
 			stz = new StringTokenizer(br.readLine());
 			for(int j = 0; j < n; j++) {
 				a[i][j] = Integer.parseInt(stz.nextToken());
-				map[i][j] = new Tree(5);
+				map[i][j] = 5;
 			}
 		}
+
+		LinkedList<Tree> list = new LinkedList<>();
 
 		for(int i = 0; i < m; i++) {
 			stz = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(stz.nextToken())-1;
 			int y = Integer.parseInt(stz.nextToken())-1;
 			int z = Integer.parseInt(stz.nextToken());
-			map[x][y].plant(z);
+			list.add(new Tree(x, y, z, true));
 		}
 
 		for(int l = 0; l < k; l++) {
-			for(int i = 0; i < n; i++) {
-				for(int j = 0; j < n; j++) {
-					map[i][j].sort();
-					map[i][j].spSummer(); 
-					map[i][j].nut += a[i][j];
-				System.out.print(map[i][j].al.size()+" ");
+			for(Tree now : list) {
+				if(map[now.x][now.y] >= now.age) {
+					map[now.x][now.y] -= now.age;
+					now.age++;
 				}
-				System.out.println();
+				else
+					now.alive = false;
 			}
-				System.out.println();
-			for(int i = 0; i < n; i++) {
-				for(int j = 0; j < n; j++) {
-					if(map[i][j].fall()) 
-						sex(i, j);
+
+			for(Iterator<Tree> it = list.iterator(); it.hasNext(); ) {
+				Tree now = it.next();
+				if(!now.alive) {
+					map[now.x][now.y] += now.age/2;
+					it.remove();
 				}
 			}
+
+			LinkedList<Tree> newList = new LinkedList<>();
+			for(Iterator<Tree> it = list.iterator(); it.hasNext();) {
+				Tree now = it.next();
+				if(now.age % 5 == 0) {
+					for(int i = -1; i <= 1; i++) {
+						for(int j = -1; j <= 1; j++) {
+							int nx = now.x + i;
+							int ny = now.y + j;
+							if(nx == now.x && ny == now.y)
+								continue;
+
+							if(nx >= 0 && ny >= 0 && nx < n && ny < n)
+								newList.add(new Tree(nx, ny, 1, true));
+						}
+					}
+				}
+			}
+
+			list.addAll(0, newList);
+
+			for(int i = 0; i < n; i++) 
+				for(int j = 0; j < n; j++)
+					map[i][j] += a[i][j];
 		}
-
-			System.out.println();
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				answer += map[i][j].al.size();
-				System.out.print(map[i][j].al.size()+" ");
-			}
-			System.out.println();
-			}
-
-		System.out.println(answer);
-	}
-
-	public static void sex(int x, int y) {
-		for(int i = -1; i <= 1; i++) {
-			for(int j = -1; j <= 1; j++) {
-				int nx = x + i;
-				int ny = y + j;
-				if(nx == x && ny == y)
-					continue;
-
-				if(nx >= 0 && nx < n && ny < n && ny >= 0) {
-					map[nx][ny].al.add(0, 1);
-				}
-			}
-		}
+		
+		System.out.println(list.size());
 	}
 
 	static class Tree {
-		int nut = 0;
-		List<Integer> al = new ArrayList<Integer>();
+		int x, y, age;
+		boolean alive;
 
-		Tree(int n) {
-			nut = n;
-		}
-
-		void plant(int z) {
-			al.add(z);
-		}
-
-		void sort() {
-			Collections.sort(al);
-		}
-
-		void spSummer() {
-			int count = 0;
-			int index = -1;
-
-			for(int i = 0; i < al.size(); i++) {
-				int now = al.get(i);
-				if(nut >= now) {
-					nut -= now;
-					al.set(i, now+1);
-				}
-				else {
-					if(index == -1)
-						index = i;
-					count += al.get(i)/2;
-				}
-			}
-
-			if(index != -1) {
-				al = al.subList(0, index);
-			}
-			nut += count/2;
-		}
-
-		boolean fall() {
-			for(int i = 0; i < al.size(); i++)
-				if(al.get(i) % 5 == 0)
-					return true;
-
-			return false;
+		Tree(int x, int y, int age, boolean alive) {
+			this.x = x;
+			this.y = y;
+			this.age = age;
+			this.alive = alive;
 		}
 
 	}
